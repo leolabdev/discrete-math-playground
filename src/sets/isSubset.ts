ee0901204ee56ee86d6d89c0defbb31fc9034583
leolab <leohannokainen999@gmail.com>
2026-02-07 13:44:26 +0200

    add

--- src/sets/isSubset.ts
+++ src/sets/isSubset.ts
@@ -3,6 +3,8 @@
  * Returns true if every element of A is contained in B.
  */
 export function isSubset<T>(A: Set<T>, B: Set<T>): boolean {
-  // TODO: implement
-  throw new Error('Not implemented');
+  for (const a of A) {
+    if (!B.has(a)) return false;
+  }
+  return true;
 }
